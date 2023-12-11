5024a17aaf8827c9506f1566ee2953352647c629
Nika <nikam2341@gmail.com>
2023-12-11 11:19:12 +0500

    add front

--- internetmagazine/urls.py
+++ internetmagazine/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from testapp import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('testapp.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('registration/', views.registration)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
